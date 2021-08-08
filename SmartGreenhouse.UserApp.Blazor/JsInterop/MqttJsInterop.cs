0106fcd38ef0fb167b62c4d6e00bde4b6240fa5b
Michal-Sitarz <me@michalsitarz.com>
2021-08-08 15:14:30 +0100

    Add MQTT broker (using paho js library)

---
+++ SmartGreenhouse.UserApp.Blazor/JsInterop/MqttJsInterop.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.JSInterop;
+
+namespace SmartGreenhouse.UserApp.Blazor.JsInterop
+{
+    public class MqttJsInterop
+    {
+        private readonly IJSRuntime _jsRuntime;
+
+        public MqttJsInterop(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+
+        public async void CreateClient(string wsHost, int wsPort, string clientId)
+        {
+            await _jsRuntime.InvokeVoidAsync(
+                "mqttFunctions.createClient",
+                wsHost, wsPort, clientId);
+        }
+
+        public async void Connect(string topic, int qos, int timeout)
+        {
+            await _jsRuntime.InvokeVoidAsync(
+                "mqttFunctions.connect",
+                topic, qos, timeout);
+        }
+
+        public async void Disconnect()
+        {
+            await _jsRuntime.InvokeVoidAsync(
+                "mqttFunctions.disconnect");
+        }
+
+        public async void Publish(string topic, string payload, int qos, bool retained)
+        {
+            await _jsRuntime.InvokeVoidAsync(
+                "mqttFunctions.publish",
+                topic, payload, qos, retained);
+        }
+    }
+}
--- SmartGreenhouse.UserApp.Blazor/Program.cs
+++ SmartGreenhouse.UserApp.Blazor/Program.cs
@@ -18,6 +18,7 @@ namespace SmartGreenhouse.UserApp.Blazor
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped<JsInterop.MqttJsInterop>();
 
             await builder.Build().RunAsync();
         }
